0352d36efe5991b8ae572c13bcd69267b888fa7d
Vishalsai Daswani <vishalsai@gmail.com>
2016-01-10 16:37:54 -0800

    changed this to instead use an encoded version of the string
    don't create a separate priority array. instead, just map it to a
    different word, such that it matches standard lexicographic sorting. and
    then return it to the original word later.

--- lexico_sorting.py
+++ lexico_sorting.py
@@ -1,28 +1,28 @@
 # Summary: Write a function to sort an array of strings based on an arbitrary lexicographic ordering.
 # The function will take two parameters: an array of strings to sort and a string specifying the lexicographic order.
 
-def get_priority(word, priority_map):
-    # input:  word = 'abc', priority_map = {'a': 1, 'b': 3, 'c': 2}
-    # output: ('abc', [1, 3, 2])
-    priority_val = map(lambda char: priority_map[char], list(word))
-    return (word, priority_val)
+def get_new_word(word, priority_map):
+    new_cs = map(lambda char: priority_map[char], list(word))
+    return ''.join(new_cs)
 
 def lexico_sorting(arr_words, str_lexico):
     # len(array_of_inputs) = n
     # len(str_lexico) = k
-    # requires O(k) memory for the priority map. in this case, k = 26. general case, bounded by # of ascii characters.
-    # O(total number of letters) memory additional - this requires more memory because we're storing priorities for each of the words
+    # memory: O(2k + 2n) = O(k+n) ~= O(n)
     # requires O(log(n)) runtime - for python sorting functionality
 
     # create priority map
     priority_map = {}
+    priority_map_reverse = {}
     for i in xrange(len(str_lexico)):
         c = str_lexico[i]
-        priority_map[c] = i
+        new_c = chr(ord('a') + i)
+        priority_map[c] = new_c
+        priority_map_reverse[new_c] = c
 
-    arr_priorities = map(lambda word: get_priority(word, priority_map), arr_words)
-    arr_priorities_sorted = sorted(arr_priorities, key=lambda priority: priority[1])
-    return map(lambda priority: priority[0], arr_priorities_sorted)
+    new_arr_words = map(lambda word: get_new_word(word, priority_map), arr_words)
+    new_arr_words.sort()
+    return map(lambda word: get_new_word(word, priority_map_reverse), new_arr_words)
 
 
 # Given Tests
